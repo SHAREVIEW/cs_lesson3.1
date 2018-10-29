0ff1cd7d858763ed60f638d5125303e3bc5c69db
david <2657070472@qq.com>
2018-10-29 13:24:47 +0800

    goto语句

--- cs_lesson3.1/Program.cs
+++ cs_lesson3.1/Program.cs
@@ -10,6 +10,22 @@ namespace cs_lesson3._1
     {
         static void Main(string[] args)
         {
+
+        //goto语句
+        start:
+        int myInteger = 5;
+        goto addVa1;
+        writeResult:
+        Console.WriteLine("myInteger = {0}", myInteger );
+        goto start;
+        addVa1:
+        myInteger += 10;
+        goto writeResult;
+
+
+
+
+
         }
     }
 }
